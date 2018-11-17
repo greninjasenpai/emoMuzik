cead78b9fc03c5a0152e93c54d15bab9a10925c0
anurang bhuta <anurang@g.ucla.edu>
2018-11-17 15:52:50 -0800

    bug_fix

--- moozik.py
+++ moozik.py
@@ -5,14 +5,17 @@ import threading
 current_emotion=""
 
 
-anger_play_list=[]
+
 contempt_play_list=[]
 disgust_play_list=[]
 fear_play_list=[]
 happiness_play_list=[]
 neutral_play_list=[]
-sadness_play_list=[]
-surprise_play_list=[]
+
+sadness_play_list=["https://www.youtube.com/watch?v=N-nn4IdkFek","https://www.youtube.com/watch?v=12SImwAMEUk","https://www.youtube.com/watch?v=IMW0QGEdxmg","https://www.youtube.com/watch?v=9qIIgwmq7-4","https://www.youtube.com/watch?v=K6Ad1_Eq4Q8","https://www.youtube.com/watch?v=UoZP3L03Rfo","https://www.youtube.com/watch?v=5BM6bGkmNY8"]
+
+surprise_play_list=["https://www.youtube.com/watch?v=GFegrI8AFUc","https://www.youtube.com/watch?v=JQSlA3pkViw","https://www.youtube.com/watch?v=36Gl8JMA7K0","https://www.youtube.com/watch?v=Kbi8CIGQ35w","https://www.youtube.com/watch?v=I8tespYBHGU","https://www.youtube.com/watch?v=sBjN0cgko6Q","https://www.youtube.com/watch?v=_5rFL8dtjuY","https://www.youtube.com/watch?v=S6JoSF_xyo0"]
+anger_play_list=[]
 
 emotion_to_playlist={
         "anger":anger_play_list,
