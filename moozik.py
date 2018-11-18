aeea8a6409dbb75dfb3565316df983d068938a68
Myank Singhal <myanksinghal@s-169-232-238-21.resnet.ucla.edu>
2018-11-17 16:44:46 -0800

    all playlists updated

--- moozik.py
+++ moozik.py
@@ -8,19 +8,19 @@ current_emotion="sadness"
 
 contempt_play_list=[]
 disgust_play_list=[]
-fear_play_list=["https://www.youtube.com/watch?v=UI2WuKFX7u0","https://www.youtube.com/watch?v=f8fUFmjqXZo","https://www.youtube.com/watch?v=0ewQiU3q5jM","https://www.youtube.com/watch?v=kVpPlWlHMrA","https://www.youtube.com/watch?v=wVodyoHhNto"]
-happiness_play_list=[]
-neutral_play_list=[]
+fear_play_list=["https://www.youtube.com/watch?v=UI2WuKFX7u0","https://www.youtube.com/watch?v=f8fUFmjqXZo","https://www.youtube.com/watch?v=0ewQiU3q5jM","https://www.youtube.com/watch?v=kVpPlWlHMrA","https://www.youtube.com/watch?v=wVodyoHhNto","https://www.youtube.com/watch?v=WcuRkaqxEAU"]
+happiness_play_list=["https://www.youtube.com/watch?v=WTht2pC-9AA","https://www.youtube.com/watch?v=wnJ6To0qkBU","https://www.youtube.com/watch?v=q0GrB07rBSk","https://www.youtube.com/watch?v=kEqPDKPoY3g","https://www.youtube.com/watch?v=NtPVFyqjUoo"]
+neutral_play_list=["https://www.youtube.com/watch?v=hR5lIKHLuPA","https://www.youtube.com/watch?v=nPjvp5u0UN8","https://www.youtube.com/watch?v=CcYq0aeTBr4","https://www.youtube.com/watch?v=Ptt_WQk4Yt8","https://www.youtube.com/watch?v=87wAZbQWsko"]
 
-sadness_play_list=["https://www.youtube.com/watch?v=N-nn4IdkFek","https://www.youtube.com/watch?v=12SImwAMEUk","https://www.youtube.com/watch?v=IMW0QGEdxmg","https://www.youtube.com/watch?v=9qIIgwmq7-4","https://www.youtube.com/watch?v=K6Ad1_Eq4Q8","https://www.youtube.com/watch?v=UoZP3L03Rfo","https://www.youtube.com/watch?v=5BM6bGkmNY8"]
+sadness_play_list=["https://www.youtube.com/watch?v=12SImwAMEUk","https://www.youtube.com/watch?v=IMW0QGEdxmg","https://www.youtube.com/watch?v=9qIIgwmq7-4","https://www.youtube.com/watch?v=K6Ad1_Eq4Q8","https://www.youtube.com/watch?v=UoZP3L03Rfo","https://www.youtube.com/watch?v=5BM6bGkmNY8"]
 
 surprise_play_list=["https://www.youtube.com/watch?v=GFegrI8AFUc","https://www.youtube.com/watch?v=JQSlA3pkViw","https://www.youtube.com/watch?v=36Gl8JMA7K0","https://www.youtube.com/watch?v=Kbi8CIGQ35w","https://www.youtube.com/watch?v=I8tespYBHGU","https://www.youtube.com/watch?v=sBjN0cgko6Q","https://www.youtube.com/watch?v=_5rFL8dtjuY","https://www.youtube.com/watch?v=S6JoSF_xyo0"]
 anger_play_list=[]
 
 emotion_to_playlist={
-        "anger":anger_play_list,
-        "contempt":contempt_play_list,
-        "disgust":disgust_play_list,
+        "anger":neutral_play_list,
+        "contempt":neutral_play_list,
+        "disgust":neutral_play_list,
         "fear":fear_play_list,
         "happiness":happiness_play_list,
         "neutral":neutral_play_list,
